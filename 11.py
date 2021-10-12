a49932ccc09321cbb36d7d7afc934df6979a3c50
Gaurav Borse <46493526+Mrlifewarior@users.noreply.github.com>
2021-10-12 17:57:42 +0530

    Python Program to Generate a Random Number
    Program to generate a random number between 0 and 9

---
+++ 11.py
@@ -0,0 +1,9 @@
+# Python Program to Generate a Random Number
+# To generate random number in Python, randint() function is used. This function is defined in random module.
+
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(0,9))
